fee4fef7106058750f2a8334e989dfb894b0856c
Purewhite Liu <purewhite@sjtu.edu.cn>
2021-05-18 19:42:02 +0800

    Part A: finished.

--- csim.c
+++ csim.c
@@ -3,6 +3,7 @@
 //              simulates the hit/miss behavior of a cache memory on this trace, 
 //              and outputs the total number of hits, misses, and evictions.
 // Author: Qi Liu (519021910529, purewhite@sjtu.edu.cn)
+
 #include <unistd.h>
 #include <getopt.h>
 #include <stdlib.h>
@@ -13,6 +14,7 @@
 #include "cachelab.h"
 
 #define ADDR_LENGTH 64
+#define USE_LRU
 
 typedef uint64_t content_t;
 
@@ -35,31 +37,32 @@ char *HELPER_INFO[] = {
 "\t•-t <tracefile>: Name of the valgrind trace to replay"
 };
 
-int t = -1, s = -1, b = -1, e = -1;
+int t = -1, s = -1, b = -1, e = -1, isVerbose = 0;;
 int num_set = -1, size_block = -1;
+int hit_cnt = 0, miss_cnt = 0, evict_cnt = 0, cur_timer = 0;
 
 uint64_t get_offset(uint64_t addr){
-    return addr & ((1<<(b+1))-1);
+    return addr & ((1<<b)-1);
 }
 
 uint64_t get_index(uint64_t addr){
-    return (addr >> b) & ((1<<(s+1))-1);
+    return (addr >> b) & ((1<<s)-1);
 }
 
 uint64_t get_tag(uint64_t addr){
-    return (addr >> (s+b)) & ((1<<(t+1))-1);
+    return (addr >> (s+b));
 }
 
-//   S 7fefe05a8,8
+void caccess(cache_t cache, uint64_t addr, int cnt);
 
 int main(int argc, char *argv[])
 {
-    int opt = 0, isVerbose = 0;
+    int opt = 0;
     char *tracefile = NULL;
     cache_t cache = NULL;
     FILE *fd = NULL;
     uint64_t addr;
-    char *cmd;
+    char *cmd = NULL;
     int cnt;
     size_t len;
 
@@ -117,19 +120,7 @@ int main(int argc, char *argv[])
         cache[i] = (block_t*)malloc(sizeof(block_t) * e);
         memset(cache[i], 0, sizeof(block_t) * e);
     }
-//   S 7fefe05a8,8
-    long curlen = 0;
-    while(1){
-        // fscanf(fd, "%c%lx,%d", &cmd, &addr, &cnt);
-        printf("begin.\n");
-        fflush(stdout);
-        if(getline(&cmd, &len, fd) == -1)
-            break;
-        fseek(fd, (curlen += len), SEEK_SET);
-        printf("read ok.");
-        fflush(stdout);
-        printf("at %ld:", ftell(fd));
-        fflush(stdout);
+    while(getline(&cmd, &len, fd) != -1){
         if(cmd[0] != ' ') continue;
         addr = 0;
         int i = 2;
@@ -137,20 +128,76 @@ int main(int argc, char *argv[])
         while(i < len && isalnum(cmd[i])){
             addr <<= 4;
             if(isdigit(cmd[i]))
-                addr |= (cmd[i]-'0');
-            else
-                addr |= (cmd[i]-'A'+10);
+                addr |= (uint8_t)(cmd[i]-'0');
+            else if(isalpha(cmd[i]))
+                addr |= (uint8_t)(tolower(cmd[i])-'a' + 10);
             ++i;
         }
         while(i < len && !isalnum(cmd[i])) ++i;
         cnt = atoi(cmd+i);
-
-        printf("at %ld: %c %lx %d\n", ftell(fd), cmd[1], addr, cnt);
-        fflush(stdout);
-        getchar();
+        if(isVerbose){
+            printf("%c %lx, %d ", cmd[1], addr, cnt);
+        }
+        switch(cmd[1]){
+            case 'M':
+                caccess(cache, addr, cnt);
+            case 'L':
+            case 'S':
+                caccess(cache, addr, cnt);
+                putchar('\n');
+                break;
+            default:
+                fprintf(stderr, "Error: invalid command \"%c\"\n", cmd[1]);
+                exit(EXIT_FAILURE);
+        }
+        free(cmd);
+        cmd = NULL;
     }
-    printf("out\n");
-    fflush(stdout);
-    // printSummary(0, 0, 0);
+    printSummary(hit_cnt, miss_cnt, evict_cnt);
+    // printf("hit_cnt:%d miss_cnt:%d evict_cnt:%d\n", hit_cnt, miss_cnt, evict_cnt);
+
+    fclose(fd);
     return 0;
+}
+
+void caccess(cache_t cache, uint64_t addr, int cnt){
+    uint64_t tag = get_tag(addr), index = get_index(addr), offset = get_offset(addr);
+    ++cur_timer;
+    // printf("tag: %lx, index: %lx, offset: %lx\n", tag, index, offset);
+    fflush(stdout);
+    for(int i = 0; i < e; ++i){
+        if(cache[index][i].tag == tag && cache[index][i].valid){
+#ifdef USE_LRU
+            cache[index][i].timer = cur_timer;
+#endif
+            ++hit_cnt;
+            if(isVerbose)
+                printf("hit ");
+            return;
+        }
+    }
+    ++miss_cnt;
+    if(isVerbose)
+        printf("miss ");
+    int place = 0;
+    while(place < e && cache[index][place].valid) ++place;
+    if(place < e){
+        cache[index][place].tag = tag;
+        cache[index][place].timer = cur_timer;
+        cache[index][place].content = 0;
+        cache[index][place].valid = 1;
+    }
+    else{
+        ++evict_cnt;
+        if(isVerbose)
+            printf("eviction ");
+        place = 0;
+        for(int i = 1; i < e; ++i)
+            if(cache[index][i].timer < cache[index][place].timer)
+                place = i;
+        cache[index][place].tag = tag;
+        cache[index][place].timer = cur_timer;
+        cache[index][place].content = 0;
+        cache[index][place].valid = 1;
+    }
 }
