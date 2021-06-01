6b1065b672296b5c60297157fe66db51034c6231
Purewhite Liu <purewhite@sjtu.edu.cn>
2021-06-01 23:17:51 +0800

    part B: 1427/1300

--- loop_unrooling.cpp
+++ loop_unrooling.cpp
@@ -1,40 +1,64 @@
 #include <cstdio>
 
-#define THRESHOLD 8
+#define THRESHOLD 4
 
 int main(){
-    for(int k = 0; k < THRESHOLD; ++k){
-        for(int l = 0; l < THRESHOLD; ++l)
-            printf("c%d = A[ii*THRESHOLD+%d][jj*THRESHOLD+%d];\n", l, k, l);
-        for(int l = 0; l < THRESHOLD; ++l)
-            printf("B[jj*THRESHOLD+%d][ii*THRESHOLD+%d] = c%d;\n", l, k, l);
+    int i, j;
+
+    // for(int k = 0; k < THRESHOLD; ++k){
+    //     for(int l = 0; l < THRESHOLD; ++l)
+    //         printf("c%d = A[ii*THRESHOLD+%d][jj*THRESHOLD+%d];\n", l, k, l);
+    //     for(int l = 0; l < THRESHOLD; ++l)
+    //         printf("B[jj*THRESHOLD+%d][ii*THRESHOLD+%d] = c%d;\n", l, k, l);
+    // }
+    // putchar('\n');
+    
+    for(i = 0; i < 4; ++i){
+        for(j = 0; j < 4; ++j)
+            printf("c%d = A[ii*8+%d][jj*8+%d];\n", j, i, j);
+        for(j = 0; j < 4; ++j)
+            printf("B[jj*8+%d][ii*8+%d] = c%d;\n", j, i, j);
+    }
+    for(i = 4; i < 8; ++i){
+        for(j = 0; j < 4; ++j)
+            printf("c%d = A[ii*8+%d][jj*8+%d];\n", j, i, j);
+        for(j = 0; j < 4; ++j)
+            printf("B[jj*8+%d][ii*8+%d] = c%d;\n", j, i, j);
+    }
+    for(i = 4; i < 8; ++i){
+        for(j = 4; j < 8; ++j)
+            printf("c%d = A[ii*8+%d][jj*8+%d];\n", j, i, j);
+        for(j = 4; j < 8; ++j)
+            printf("B[jj*8+%d][ii*8+%d] = c%d;\n", j, i, j);
+    }
+    for(i = 0; i < 4; ++i){
+        for(j = 4; j < 8; ++j)
+            printf("c%d = A[ii*8+%d][jj*8+%d];\n", j, i, j);
+        for(j = 4; j < 8; ++j)
+            printf("B[jj*8+%d][ii*8+%d] = c%d;\n", j, i, j);
     }
 
-            // for(int i = 0; i < 4; ++i){
-            //     for(int j = 0; j < 4; ++j)
-            //         printf("c%d = A[ii*8+%d][jj*8+%d]", j, i, j);
-            //     for(int j = 0; j < 4; ++j)
-            //         printf("B[jj*8+%d][ii*8+%d] = c%d;\n", j, i, j);
-                
-            //     for(int j = 5; j < 8; ++j)
-            //         printf("c%d = A[ii*8+%d][jj*8+%d]", j, i, j);
-            //     for(int j = 5; j < 8; ++j)
-            //         printf("B[jj*8+%d][ii*8+%d] = c%d;\n", j, i, j);
-            // }
-
-            // for(int i = 4; i < 8; ++i){
-            //     for(int j = 5; j < 8; ++j)
-            //         printf("c%d = A[ii*8+%d][jj*8+%d]", j, i, j);
-            //     for(int j = 5; j < 8; ++j)
-            //         printf("B[jj*8+%d][ii*8+%d] = c%d;\n", j, i, j);
-
-            //     for(int j = 0; j < 4; ++j)
-            //         printf("c%d = A[ii*8+%d][jj*8+%d]", j, i, j);
-            //     for(int j = 0; j < 4; ++j)
-            //         printf("B[jj*8+%d][ii*8+%d] = c%d;\n", j, i, j);
-            // }
-
     putchar('\n');
+
+    // for(i = 0; i < 4; ++i)
+    //     for(j = 0; j < 4; ++j)
+    //         printf("B[jj*8+%d][ii*8+%d] = A[ii*8+%d][jj*8+%d];\n", j, j, i, j);
+    // putchar('\n');
+    // for(i = 4; i < 8; ++i)
+    //     for(j = 0; j < 4; ++j)
+    //         printf("B[jj*8+%d][ii*8+%d] = A[ii*8+%d][jj*8+%d];\n", j, j, i, j);
+    // putchar('\n');
+    // for(i = 4; i < 8; ++i)
+    //     for(j = 4; j < 8; ++j)
+    //         printf("B[jj*8+%d][ii*8+%d] = A[ii*8+%d][jj*8+%d];\n", j, j, i, j);
+    // putchar('\n');
+    // for(i = 0; i < 4; ++i)
+    //     for(j = 4; j < 8; ++j)
+    //         printf("B[jj*8+%d][ii*8+%d] = A[ii*8+%d][jj*8+%d];\n", j, j, i, j);
+    // putchar('\n');
+
+
+
     for(int k = 0; k < THRESHOLD; ++k)
         printf("c%d = A[ii*THRESHOLD+%d][j];\n", k, k);
     for(int k = 0; k < THRESHOLD; ++k)
